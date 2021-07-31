fa39f2521eb454402e880474b661823c1b86bdf9
kanchan-solanke <kanchanssolanke@gmail.com>
2021-07-31 07:42:12 +0530

    13.Create the Week Object having a list of WeekDay objects each storing the day

--- Data_Structure.java
+++ Data_Structure.java
@@ -1,70 +1,45 @@
-public class WeekDay{
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
-    class Node
+class DayofWeek {
+    static int method(int year, int month, int day)
     {
-        String data;
-        Node next;
-    }
-    Node front=null;
-    Node rear=null;
-    public boolean isEmpty()
-    {
-        if(front==null &&rear==null)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
-    public void enqueue(String data)
-    {
-        Node node=new Node();
-        node.data=data;
-        node.next=null;
-        if(front==null)
-        {
-            front=node;
-            rear=node;
-        }
-        else
-        {
-            rear.next=node;
-            rear=node;
-        }
-    }
-    public String dequeue()
-    {
-        if(isEmpty())
-        {
-            System.out.println("Underflow");
-            return  "";
-        }
-        else
-        {
-            Node node=front;
-            String no= node.data;
-            front=front.next;
-            return no;
-        }
-    }
-    public void display()
-    {
-        if(isEmpty())
-        {
-            System.out.println("Underflow");
-        }
-        else
-        {
-            Node node=front;
-            while(node!=null)
-            {
-                System.out.println(node.data);
-                node=node.next;
+        // to check the validity of day of month
+        int[] monthends = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+        // check validity of year and month
+        if (year > 0 && (month > 0 && month <= 12)) {
+
+            // check for a leap year and month of February
+
+            if ((year % 4 == 0 || year % 400 == 0) &&
+                    month == 2 && (day > 0 && day <= 29)) {
+
+                // month starts at zero, so we need to put ( month - 1 ).
+                Calendar calendar = new GregorianCalendar(year, month - 1, day);
+                return calendar.get(Calendar.DAY_OF_WEEK);
+            }
+            else {
+
+                // check the range of day of month
+                if (day > 0 && day <= monthends[month - 1]) {
+
+                    // month starts at zero, so we need to put ( month - 1 ).
+                    Calendar calendar = new GregorianCalendar(year, month - 1, day);
+                    return calendar.get(Calendar.DAY_OF_WEEK);
+                }
+                else
+                    return 0;
             }
         }
+        else
+            return 0;
     }
-
-
+    public static void main(String[] args)
+    {
+        String[] dayofweek = { "Invalid", "Sun", "Mon", "Tue",
+                "Wed", "Thur", "Fri", "Sat" };
+        int y = 2018, m = 3, d = 26;            // assume values
+         System.out.println(dayofweek[method(y, m, d)]);
+    }
 }
